4d695b826106b41e7c309b8cbd1a451eb75132c5
soloking2 <soloking2forsure@gmail.com>
2019-09-25 09:58:42 +0100

    added angular project

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from  '@angular/router';
+import { IProduct } from './products';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Garden Cart',
+      'productCode': 'GDN-0023',
+      'releaseDate': 'March 18, 2019',
+      'description': '15 gallon capacity rolling garden cart',
+      'price': 32.99,
+      'starRating': 4.2,
+      'imageUrl': 'assets/images/garden_cart.png'
+    }
+
+  }
+
+  onBack(): void{
+    this.router.navigate(['/products']);
+  }
+
+}
